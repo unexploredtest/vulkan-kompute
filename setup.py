6e74cd06c8e5760ef454b2cd56f866cd28ceabde
unexplored test <unexploredtest@protonmail.com>
2021-04-04 13:03:36 +0430

    Revert "changed setup.py so that some extention would be resolved"
    This reverts commit f83f9c53c6b540323a6c39b055acd8d97269b94f.

--- setup.py
+++ setup.py
@@ -12,29 +12,6 @@ curr_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(curr_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-def patched_WindowsPlatform_init(self):
-	import textwrap
-	from skbuild.platform_specifics.windows import WindowsPlatform, CMakeVisualStudioCommandLineGenerator, CMakeVisualStudioIDEGenerator
-
-	super(WindowsPlatform, self).__init__()
-
-	self._vs_help = textwrap.dedent("""
-		Building Windows wheels for requires Microsoft Visual Studio 2017 or 2019:
-		  https://visualstudio.microsoft.com/vs/
-		""").strip()
-
-	supported_vs_years = [("2019", "v141"), ("2017", "v141")]
-	for vs_year, vs_toolset in supported_vs_years:
-		self.default_generators.extend([
-			CMakeVisualStudioCommandLineGenerator("Ninja", vs_year, vs_toolset),
-			CMakeVisualStudioIDEGenerator(vs_year, vs_toolset),
-			CMakeVisualStudioCommandLineGenerator("NMake Makefiles", vs_year, vs_toolset),
-			CMakeVisualStudioCommandLineGenerator("NMake Makefiles JOM", vs_year, vs_toolset)
-		])
-
-import skbuild.platform_specifics.windows
-skbuild.platform_specifics.windows.WindowsPlatform.__init__ = patched_WindowsPlatform_init
-
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
